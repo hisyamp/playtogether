d62239ddff03d0c3c5b423dd78626470665fd778
hisyam priaji <hisyampriaji.hp@gmail.com>
2022-10-10 21:11:11 +0700

    a

--- start/routes.ts
+++ start/routes.ts
@@ -23,7 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
-
+ 
 Route.group(() => {
   Route.post('/register', 'AuthController.register')
   Route.post('/login', 'AuthController.login')
